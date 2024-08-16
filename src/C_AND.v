d27a1b518896d8bfad246387f3928155afed6503
RodriRosario <jose.rl@matamoros.tecnm.mx>
2024-08-15 19:02:42 -0500

    Add files via upload

---
+++ src/C_AND.v
@@ -0,0 +1,8 @@
+module C_AND (
+  input A,
+  input B,
+  output OUT
+);
+  
+  assign OUT=A&B;
+endmodule
---
+++ src/C_XOR.v
@@ -0,0 +1,9 @@
+module C_XOR (
+  input A,
+  input B,
+  output OUT
+);
+  
+  assign OUT=A^B;
+endmodule
+ 
